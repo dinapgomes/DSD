f4420bfa2d957dac241b83ed7a983d555189e308
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-01-17 13:35:40 +0000

    Adicionando condições de execução

--- Gateway/api-gateway/index.js
+++ Gateway/api-gateway/index.js
@@ -3,20 +3,22 @@ const httpProxy = require('express-http-proxy');
 const express = require('express');
 const app = express();
 var logger = require('morgan');
- 
+
 app.use(logger('dev'));
- 
+
 function selectProxyHost(req) {
-    if (req.path.startsWith('/movies'))
-        return 'http://localhost:3000/';
-    else if (req.path.startsWith('/cinemas'))
-        return 'http://localhost:3001/';
+    if (req.path.startsWith('/clube')){
+        return 'http://localhost:3001/clube';
+    }
+    else
+     return 'http://localhost:3001/pais';
+   
 }
- 
+
 app.use((req, res, next) => {
     httpProxy(selectProxyHost(req))(req, res, next);
 });
- 
+
 app.listen(10000, () => {
     console.log('API Gateway executando!');
 });
